bffc566f4564df45b412192ee7f6ecb0e7ff4c63
Sergey Feldsherov <imnosuperman74@gmail.com>
2024-09-16 16:47:43 +0300

    Improved implementaiton of naive innerproduct benchmark
    Use Range to test with varying arguments, add complexity verification
    check.

--- innerproduct/bmip.cpp
+++ innerproduct/bmip.cpp
@@ -4,21 +4,24 @@
 
 #include "ipfun.h"
 
-constexpr int d = 1024;
-
 static void BM_ip_naive(benchmark::State &state) {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<float> uni(0, 1);
-    float v[d], w[d];
+    int d = (int)state.range(0);
+    float *v, *w;
+    v = new float[d];
+    w = new float[d];
     for (int i = 0; i < d; ++i) {
         v[i] = uni(mt);
         w[i] = uni(mt);
     }
     for (auto _ : state) {
-        ip_naive(v, w, d);
+        float d_ip = ip_naive(v, w, d);
+        benchmark::DoNotOptimize(d_ip);
     }
+    state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_ip_naive);
+BENCHMARK(BM_ip_naive)->Range(64, 4096*8)->Complexity(benchmark::oN);
 
 BENCHMARK_MAIN();
