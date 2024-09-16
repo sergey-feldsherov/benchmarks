27b4c92746aba1149aabcd0a72c7a3f7ee3ac86d
Sergey Feldsherov <imnosuperman74@gmail.com>
2024-09-16 15:58:56 +0300

    Improved implementation of benchmark
    Run tests with a sparse set of arguments from an interval, compute
    asymptotic complexity (verify).

--- pow2rd/bmpow2rd.cpp
+++ pow2rd/bmpow2rd.cpp
@@ -2,20 +2,27 @@
 
 #include "pow2rd.h"
 
+uint32_t shortnum = 1940712347;
+uint32_t longnum = 2340712347;  // runs for eternity in Debug builds
+
+uint32_t num = shortnum;
+
 static void BM_pow2rd_naive(benchmark::State &state) {
-    int a = 42;
     for (auto _ : state) {
-        pow2rd_naive(a);
+        int result = pow2rd_naive((uint32_t)state.range(0));
+        benchmark::DoNotOptimize(result);
     }
+    state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_pow2rd_naive);
+BENCHMARK(BM_pow2rd_naive)->Range(7, shortnum)->Complexity(benchmark::oLogN);
 
 static void BM_pow2rd_builtin_clz(benchmark::State &state) {
-    int a = 42;
     for (auto _ : state) {
-        pow2rd_builtin_clz(a);
+        int result = pow2rd_builtin_clz((uint32_t)state.range(0));
+        benchmark::DoNotOptimize(result);
     }
+    state.SetComplexityN(state.range(0));
 }
-BENCHMARK(BM_pow2rd_builtin_clz);
+BENCHMARK(BM_pow2rd_builtin_clz)->Range(7, shortnum)->Complexity(benchmark::o1);
 
 BENCHMARK_MAIN();
