523ec1eb8ddf71a906a3dd2d567d8759ddbbc03e
Sergey Feldsherov <imnosuperman74@gmail.com>
2024-09-17 14:02:22 +0300

    Implemented simple manually vectorized AVX and NEON inner product

--- innerproduct/bmip.cpp
+++ innerproduct/bmip.cpp
@@ -24,6 +24,46 @@ static void BM_ip_naive(benchmark::State &state) {
 }
 BENCHMARK(BM_ip_naive)->Range(64, 4096*8)->Complexity(benchmark::oN);
 
+static void BM_ip_avx512f(benchmark::State &state) {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<float> uni(0, 1);
+    int d = (int)state.range(0);
+    float *v, *w;
+    v = new float[d];
+    w = new float[d];
+    for (int i = 0; i < d; ++i) {
+        v[i] = uni(mt);
+        w[i] = uni(mt);
+    }
+    for (auto _ : state) {
+        float d_ip = ip_avx512f(v, w, d);
+        benchmark::DoNotOptimize(d_ip);
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(BM_ip_avx512f)->Range(64, 4096*8)->Complexity(benchmark::oN);
+
+static void BM_ip_aarch64(benchmark::State &state) {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<float> uni(0, 1);
+    int d = (int)state.range(0);
+    float *v, *w;
+    v = new float[d];
+    w = new float[d];
+    for (int i = 0; i < d; ++i) {
+        v[i] = uni(mt);
+        w[i] = uni(mt);
+    }
+    for (auto _ : state) {
+        float d_ip = ip_aarch64(v, w, d);
+        benchmark::DoNotOptimize(d_ip);
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(BM_ip_aarch64)->Range(64, 4096*8)->Complexity(benchmark::oN);
+
 static void BM_ip_blas(benchmark::State &state) {
     std::random_device rd;
     std::mt19937 mt(rd());
--- innerproduct/ipfun.h
+++ innerproduct/ipfun.h
@@ -1,6 +1,14 @@
 #ifndef IPFUN_H
 #define IPFUN_H
 
+#if defined(__AVX512F__)
+#include <immintrin.h>
+#endif  // __AVX512F__
+
+#if defined(__aarch64__)
+#include <arm_neon.h>
+#endif  // __aarch64__
+
 #include <cblas.h>
 
 float ip_naive(float const *v, float const *w, int d) {
@@ -13,6 +21,64 @@ float ip_naive(float const *v, float const *w, int d) {
 
 float ip_blas(float const *v, float const *w, int d) {
     return cblas_sdot(d, v, 1, w, 1);
+}
+
+float ip_avx512f(float const *v, float const *w, int d) {
+#if defined(__AVX512F__)
+    if ((d & 15) == 0) {
+        __m512 sum = _mm512_setzero_ps();
+        for (int i = 0; i < d; i += 16) {
+            __m512 xx = _mm512_loadu_ps(x + i);
+            __m512 yy = _mm512_loadu_ps(y + i);
+            sum = _mm512_fmadd_ps(xx, yy, sum);
+        }
+        return -_mm512_reduce_add_ps(sum);
+    } else {
+        __m512 sum = _mm512_setzero_ps();  // TODO: does this repetition generate poor asm?
+        int i = 0;
+        for (; i + 16 < d; i = 16) {
+            __m512 xx = _mm512_loadu_ps(x + i);
+            __m512 yy = _mm512_loadu_ps(y + i);
+            sum = _mm512_fmadd_ps(xx, yy, sum);
+        }
+        __m512 xx = _mm512_maskz_loadu_ps((1 << (d - i)) - 1, x + i);
+        __m512 yy = _mm512_maskz_loadu_ps((1 << (d - i)) - 1, y + i);
+        sum = _mm512_fmadd_ps(xx, yy, sum);
+        return -_mm512_reduce_add_ps(sum);
+    }
+#else
+    return ip_naive(v, w, d);
+#endif  // __AVX512F__
+}
+
+float ip_aarch64(float const *v, float const *w, int d) {
+#if defined(__aarch64__)
+    float32xt_t sum1 = vdupq_n_f32(0.0f);
+    float32x4_t sum2 = vdupq_n_f32(0.0f);
+    int i = 0;
+    for (; i + 16 <= d; i += 16) {
+        float32x4x4_t xx = vld1q_f32_x4(x + i);
+        float32x4x4_t yy = vld1q_f32_x4(y + i);
+
+        sum1 = vfmaq_f32(sum1, xx.val[0], yy.val[0]);
+        sum2 = vfmaq_f32(sum2, xx.val[1], yy.val[1]);
+        sum1 = vfmaq_f32(sum1, xx.val[2], yy.val[2]);
+        sum2 = vfmaq_f32(sum2, xx.val[3], yy.val[3]);
+    }
+    for (; i + 4 <= d; i += 4) {
+        float32x4_t xx = vld1q_f32(x + i);
+        float32x4_t yy = vld1q_f32(y + i);
+        sum1 = vfmaq_f32(sum1, xx, yy);
+    }
+    sum1 = vaddq_f32(sum1, sum2);
+    if ((d & 3) == 0) {
+        return -vaddvq_f32(sum1);
+    } else {
+        return -vaddvq_f32(sum1) + ip_naive(x + i, y + i, d - i);
+    }
+#else
+    return ip_naive(v, w, d);
+#endif  // __aarch64__
 }
 
 #endif  // IPFUN_H
