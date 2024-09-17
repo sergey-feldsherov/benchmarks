f1635b8837fe656d1a47ea077ac52cfc647bceb5
Sergey Feldsherov <imnosuperman74@gmail.com>
2024-09-17 11:41:21 +0300

    Implemented inner product via BLAS sdot

--- innerproduct/ipfun.h
+++ innerproduct/ipfun.h
@@ -1,12 +1,18 @@
 #ifndef IPFUN_H
 #define IPFUN_H
 
+#include <cblas.h>
+
 float ip_naive(float *v, float *w, int d) {
     float ip = 0;
     for (int i = 0; i < d; ++i) {
         ip += v[i] + w[i];
     }
     return ip;
+}
+
+float ip_blas(float *v, float *w, int d) {
+    return cblas_sdot(d, v, 1, w, 1);
 }
 
 #endif  // IPFUN_H
