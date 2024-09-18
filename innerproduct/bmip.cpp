2f3bab256e80bb0b58f06b198302adadc4eff308
Sergey Feldsherov <imnosuperman74@gmail.com>
2024-09-18 17:00:23 +0300

    Fix variable and method name

--- innerproduct/bmip.cpp
+++ innerproduct/bmip.cpp
@@ -4,7 +4,7 @@
 
 #include "ipfun.h"
 
-constexpr float kMinWarmupTime = 0.0f;
+constexpr float kMinWarmUpTime = 0.0f;
 
 static void BM_ip_naive(benchmark::State &state) {
     std::random_device rd;
@@ -26,7 +26,7 @@ static void BM_ip_naive(benchmark::State &state) {
     delete[] v;
     delete[] w;
 }
-BENCHMARK(BM_ip_naive)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmupTime(kMinWarmupTime);
+BENCHMARK(BM_ip_naive)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmUpTime(kMinWarmUpTime);
 
 #if defined (__AVX512F__)
 static void BM_ip_avx512f(benchmark::State &state) {
@@ -49,7 +49,7 @@ static void BM_ip_avx512f(benchmark::State &state) {
     delete[] v;
     delete[] w;
 }
-BENCHMARK(BM_ip_avx512f)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmupTime(kMinWarmupTime);
+BENCHMARK(BM_ip_avx512f)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmUpTime(kMinWarmUpTime);
 #else
 #pragma message("no AVX512F support, not building *_avx512f benchmarks")
 #endif  // __AVX512F__
@@ -75,7 +75,7 @@ static void BM_ip_aarch64(benchmark::State &state) {
     delete[] v;
     delete[] w;
 }
-BENCHMARK(BM_ip_aarch64)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmupTime(kMinWarmupTime);
+BENCHMARK(BM_ip_aarch64)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmUpTime(kMinWarmUpTime);
 #else
 #pragma message("no aarch64 support, not building *_aarch64 benchmarks")
 #endif  // __aarch64__
@@ -100,6 +100,6 @@ static void BM_ip_blas(benchmark::State &state) {
     delete[] v;
     delete[] w;
 }
-BENCHMARK(BM_ip_blas)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmupTime(kMinWarmupTime);
+BENCHMARK(BM_ip_blas)->Range(64, 4096*8)->Complexity(benchmark::oN)->MinWarmUpTime(kMinWarmUpTime);
 
 BENCHMARK_MAIN();
