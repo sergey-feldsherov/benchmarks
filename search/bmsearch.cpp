47dc114ea516a34c7e14db8d4900aca02d8387b3
Sergey Feldsherov <imnosuperman74@gmail.com>
2024-09-19 11:56:20 +0300

    Use unsiged integers where appropriate; fix mismatched types.

--- search/bmsearch.cpp
+++ search/bmsearch.cpp
@@ -12,15 +12,15 @@ static void BM_search_linear(benchmark::State &state) {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_int_distribution<int> uni(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
-    int d = (int)state.range(0);
+    unsigned int d = (unsigned int)state.range(0);
     int *numbers = new int[d];
-    for (int i = 0; i < d; ++i) {
+    for (unsigned int i = 0; i < d; ++i) {
         numbers[i] = uni(mt);
     }
     int target = uni(mt);
     std::sort(numbers, numbers + d);
     for (auto _ : state) {
-        int idx = search_linear(numbers, d, target);
+        unsigned int idx = search_linear(numbers, d, target);
         benchmark::DoNotOptimize(idx);
     }
     state.SetComplexityN(state.range(0));
@@ -32,15 +32,15 @@ static void BM_search_bin(benchmark::State &state) {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_int_distribution<int> uni(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
-    int d = (int)state.range(0);
+    unsigned int d = (unsigned int)state.range(0);
     int *numbers = new int[d];
-    for (int i = 0; i < d; ++i) {
+    for (unsigned int i = 0; i < d; ++i) {
         numbers[i] = uni(mt);
     }
     int target = uni(mt);
     std::sort(numbers, numbers + d);
     for (auto _ : state) {
-        int idx = search_bin(numbers, d, target);
+        unsigned int idx = search_bin(numbers, d, target);
         benchmark::DoNotOptimize(idx);
     }
     state.SetComplexityN(state.range(0));
--- search/search.hpp
+++ search/search.hpp
@@ -1,18 +1,18 @@
 #ifndef SEARCH_HPP
 #define SEARCH_HPP
 
-int search_linear(int const *numbers, int d, int target) {
-    int i = 0;
+unsigned int search_linear(int const *numbers, unsigned int d, int target) {
+    unsigned int i = 0;
     for (; i < d; ++i) {
         if (numbers[i] == target) {
             return i;
         }
     }
-    return -1;
+    return d;
 }
 
-int search_bin(int const *numbers, int d, int target) {
-    int l = 0, r = d - 1;
+unsigned int search_bin(int const *numbers, unsigned int d, int target) {
+    unsigned int l = 0, r = d - 1;
     while(l <= r) {
         int m = l + (r - l) / 2;
         if (numbers[m] == target) {
